b83e6142d176c9400a47ca19917e54f537047cb4
matthewjung04 <matthew.jung95@gmail.com>
2024-07-19 14:55:25 -0400

    update shows section of shows page with js data

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -22,28 +22,47 @@ let shows = {
 };
 
 // Select the table html tag as parent container
-let table = document.querySelector('.shows-table');
+let table = document.querySelector('.shows__table');
+
+function addRow() {
+    let newRow = document.createElement('tr');
+    table.appendChild(newRow);
+    return newRow;
+}
 
 // For loop to add each corresponding date and venue to new row of the table
 for (i=0; i<shows.dates.length; i++) {
-    // Adds an empty row to bottom of the table
-    let newRow = document.createElement('tr');
-    table.appendChild(newRow);
 
-    // Create a new column within the new row and add the matching date
+    newRow = addRow();
+    let headerDate = document.createElement('th');
+    headerDate.classList.add('shows__table__header');
+    headerDate.innerText = 'DATE'
+    newRow.appendChild(headerDate);
+
     let dateColumn = document.createElement('td');
+    dateColumn.classList.add('shows__table__data--date');
     dateColumn.innerText = shows.dates[i];
-
-    // Create a new column within the new row and add the matching venue
-    let venueColumn = document.createElement('td');
-    venueColumn.innerText = shows.venues[i];
-
-    // Create a new column within the new row and add the location
-    let locationColumn = document.createElement('td');
-    locationColumn.innerText = shows.location;
-
-    // Add the new columns to the new row
     newRow.appendChild(dateColumn);
+
+    newRow = addRow();
+    let headerVenue = document.createElement('th');
+    headerVenue.classList.add('shows__table__header');
+    headerVenue.innerText = 'VENUE'
+    newRow.appendChild(headerVenue);
+
+    let venueColumn = document.createElement('td');
+    venueColumn.classList.add('shows__table__data');
+    venueColumn.innerText = shows.venues[i];
     newRow.appendChild(venueColumn);
+
+    newRow = addRow();
+    let headerLocation = document.createElement('th');
+    headerLocation.classList.add('shows__table__header');
+    headerLocation.innerText = 'LOCATION'
+    newRow.appendChild(headerLocation);
+
+    let locationColumn = document.createElement('td');
+    locationColumn.classList.add('shows__table__data');
+    locationColumn.innerText = shows.location;
     newRow.appendChild(locationColumn);
 }

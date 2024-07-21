4dfe226785521b69691516c13a8b62d552a6929b
matthewjung04 <matthew.jung95@gmail.com>
2024-07-21 03:11:40 -0400

    add comments section to bio page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -21,9 +21,12 @@ let shows = {
     location: 'San Francisco, CA'
 };
 
+// Use DOM methods to add html element containing all data from shows object
+
 // Select the table html tag as parent container
 let table = document.querySelector('.shows__table');
 
+// Function that creates a row with corresponding styling and mouse events
 function addRow() {
     let newRow = document.createElement('tr');
     newRow.style = 'cursor: pointer';
@@ -37,43 +40,52 @@ function addRow() {
 // For loop to add each corresponding date and venue to new row of the table
 for (i=0; i<shows.dates.length; i++) {
 
+    // Add a new row to the table
     newRow = addRow();
     // newRow.setAttribute("id", "row-" + i+1);
 
+    // Create a row heading for date
     let headerDate = document.createElement('th');
     headerDate.classList.add('shows__table__header--first');
     headerDate.innerText = 'DATE'
     newRow.appendChild(headerDate);
 
+    // Add corresponding date from shows object to 'DATE' row
     let dateColumn = document.createElement('td');
     dateColumn.classList.add('shows__table__data--first');
     dateColumn.innerText = shows.dates[i];
     newRow.appendChild(dateColumn);
 
+    // Create a row heading for venue
     let headerVenue = document.createElement('th');
     headerVenue.classList.add('shows__table__header');
     headerVenue.innerText = 'VENUE'
     newRow.appendChild(headerVenue);
 
+    // Add corresponding venue from shows object to 'VENUE' row
     let venueColumn = document.createElement('td');
     venueColumn.classList.add('shows__table__data');
     venueColumn.innerText = shows.venues[i];
     newRow.appendChild(venueColumn);
     
+    // Create a row heading for location
     let headerLocation = document.createElement('th');
     headerLocation.classList.add('shows__table__header');
     headerLocation.innerText = 'LOCATION'
     newRow.appendChild(headerLocation);
 
+    // Add location 'San Francisco' from shows object to 'LOCATION' row
     let locationColumn = document.createElement('td');
     locationColumn.classList.add('shows__table__data');
     locationColumn.innerText = shows.location;
     newRow.appendChild(locationColumn);
 
+    // Create a blank bottom data row for the 'Buy Tickets' button
     let newColumn = document.createElement('td');
     newColumn.classList.add('shows__table__data--last');
     newRow.appendChild(newColumn);
 
+    // Create a button within the new bottom data row with corresponding stylings
     let button = document.createElement('button');
     button.type = 'button';
     button.style = 'cursor: pointer';
@@ -82,23 +94,29 @@ for (i=0; i<shows.dates.length; i++) {
     newColumn.appendChild(button);
 }
 
-// Add hover and click events for shows table
+// Add hover and click events for shows table based on mouse events
+
+// Select all 'tr' elements in html
 const rows = document.querySelector('tr');
 
+// Behaviour for when mouse hovers over the row
 function hoverHandler (event) {
     event.currentTarget.style.backgroundColor = '#FAFAFA';
 }
 
+// Resets the row when mouse hovers away from the row
 function hoverAwayHandler (event) {
     event.currentTarget.style.backgroundColor = '#FFFFFF';
 }
 
+// Behaviour for when row is 'clicked'
 function clickHandler (event) {
     event.currentTarget.style.backgroundColor = '#E1E1E1';
     console.log(event.target.parentElement);
     event.target.parentElement.style.backgroundColor = '#E1E1E1';
 }
 
+// Add event litseners to the 'tr' elements
 rows.addEventListener('mouseover', hoverHandler);
 rows.addEventListener('mouseout', hoverAwayHandler);
 rows.addEventListener('click', clickHandler);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,1 +1,78 @@
 // Script File for Biography Page
+
+// Create object containing default comments
+
+const defaultComments = {
+    users: [
+        'Victor Pinto',
+        'Christina Cabrera',
+        'Isaac Tadesse'
+    ],
+    comments: [
+        'This is art. This is inexplicable magic expressed in the purest way, \
+        everything that makes up this majestic work deserves reverence. \
+        Let us appreciate this for what it is and what it contains.',
+
+        'I feel blessed to have seen them in person. What a show! \
+        They were just perfection. If there was one day of my life I could relive, \
+        this would be it. What an incredible day.',
+
+        "I can't stop listening. Every time I hear one of their songs - the vocals - \
+        it gives me goosebumps. Shivers straight down my spine. \
+        What a beautiful expression of creativity. Can't get enough."
+    ],
+    dates: [
+        '11/02/2023',
+        '10/28/2023',
+        '10/20/2023',
+    ]
+};
+
+let defaultSection = document.querySelector('.comments--default');
+// avatarImage.src = "../assets/Images/Mohan-muruge.jpg";
+
+function addComment() {
+    let newComment = document.createElement('div');
+    newComment.classList.add('comments--default__box');
+    defaultSection.appendChild(newComment);
+
+    let avatarImage = document.createElement('img');
+    avatarImage.classList.add('comments--default__box__image');
+    newComment.appendChild(avatarImage);
+
+    return newComment;
+}
+
+function addTextBox(newComment) {
+    let commentBox = document.createElement('div');
+    commentBox.classList.add('comments--default__box__text');
+    newComment.appendChild(commentBox);
+
+    return commentBox
+}
+
+
+for (i=0; i<defaultComments.users.length; i++) {
+
+    newComment = addComment();
+    newTextBox = addTextBox(newComment);
+
+    let titleBox = document.createElement('h1');
+    titleBox.classList.add('comments--default__box__text__head');
+    newTextBox.appendChild(titleBox);
+
+    let commentName = document.createElement('h2');
+    commentName.classList.add('comments--default__box__text__head--name');
+    commentName.innerText = defaultComments.users[i];
+    titleBox.appendChild(commentName);
+
+    let commentDate = document.createElement('h2');
+    commentDate.classList.add('comments--default__box__text__head--date');
+    commentDate.innerText = defaultComments.dates[i];
+    titleBox.appendChild(commentDate);
+
+    let commentText = document.createElement('span');
+    commentText.classList.add('comments--default__box__text__desc');
+    commentText.innerText = defaultComments.comments[i];
+    newTextBox.appendChild(commentText);
+}
